4e82215c46f42671dca67a426747f993ea98c9a1
Дмитрий Земляков <dmitriyz@Mac-mini-Dmitrij.local>
2022-06-09 20:03:53 +0300

    feat: Add read/write streams

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import readline from "readline";
 import { chdir, cwd } from "process";
-import {EOL,arch,homedir,cpus, userInfo} from "os";
+import { EOL, arch, homedir, cpus, userInfo } from "os";
+import {createReadStream,createWriteStream } from "fs";
+
 
 import { readdir } from "fs/promises";
 
@@ -29,20 +31,42 @@ const up = (current) => {
   return cwd();
 };
 const cd = (pathToDir) => {
-    let newDir = path.join(cwd(), pathToDir)
-    chdir(newDir)
+  let newDir = path.join(cwd(), pathToDir);
+  chdir(newDir);
+};
+
+const getOSComand = (comand) => {
+  const comandsObj = {
+    EOL: JSON.stringify(EOL),
+    cpus: cpus(),
+    homedir: homedir(),
+    architecture: arch(),
+    username: userInfo().username,
+  };
+  return console.log(comandsObj[comand], EOL);
+};
+
+
+const read = (filename) =>{
+   const rs = createReadStream(filename)
+   rs.on('data', (chunk) => {
+       console.log(chunk.toString());
+   })
+   .on('error', (e) => {
+       console.log(e.message);
+   }) 
 }
 
-const getOSComand = (comand) =>{
-const comandsObj = {
-    'EOL': EOL,
-    'cpus': cpus(),
-    'homedir': homedir(),
-    'architecture': arch(),
-    'username': userInfo().username 
-}
-return console.log(comandsObj[comand],EOL)
-}
+const create = (filename) =>{
+    const ws = createWriteStream(filename)
+    ws.on('finish', ()=>{
+        console.log('file has written');
+    })
+    .on('error', (e) => {
+        console.log(e.message);
+    }) 
+    ws.end()
+ }
 const userName = process.argv.slice(-1).join("").split("=").slice(-1).join("");
 
 const rl = readline.createInterface({
@@ -52,7 +76,7 @@ const rl = readline.createInterface({
 process.chdir(homeDir);
 
 rl.question(
-  `Welcome to the File Manager, ${userName}!\n\nYou are currently in ${homeDir}\n`,
+  `Welcome to the File Manager, ${userName}!${EOL}You are currently in ${homeDir}${EOL}`,
   (answer) => {
     console.log(`Ok, ${userName}`);
   }
@@ -71,21 +95,35 @@ rl.on("line", (answer) => {
   }
 });
 rl.on("line", (answer) => {
-    if (answer.startsWith("cd")) {
-      answer = answer.split(' ').slice(-1).join('')
-        cd(answer);
-      console.log(`You are currently in: ${cwd()}`);
+  if (answer.startsWith("cd")) {
+    answer = answer.split(" ").slice(-1).join("");
+    cd(answer);
+    console.log(`You are currently in: ${cwd()}`);
+  }
+});
+rl.on("line", (answer) => {
+  if (answer.startsWith("os")) {
+    answer = answer.split(" ").slice(-1).join("").split("--").join("");
+    getOSComand(answer);
+  }
+});
+
+rl.on("line", (answer) => {
+    if (answer.startsWith("cat")) {
+      answer = answer.split(" ").slice(-1).join("");
+      const pathToFile= path.join(cwd(), answer)
+      read(pathToFile)
     }
   });
+
   rl.on("line", (answer) => {
-    if (answer.startsWith("os")) {
-      answer = answer.split(' ').slice(-1).join('').split('--').join('')
-      getOSComand(answer)
-    //     cd(answer);
-    //   console.log(`You are currently in: ${cwd()}`);
+    if (answer.startsWith("add")) {
+      answer = answer.split(" ").slice(-1).join("");
+      const pathToFile= path.join(cwd(), answer)
+      create(pathToFile)
     }
   });
 
 rl.on("close", () => {
-  console.log(`\nThank you for using File Manager, ${userName}\n`);
+  console.log(`${EOL}Thank you for using File Manager, ${userName}${EOL}`);
 });
